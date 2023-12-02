2ae6597bd2c8fecf12695d80a14bbb0dbb859cca
Sergey <Kuberunnak@ya.ru>
2023-12-02 15:57:37 +0300

    add reg form

--- online_store/store/forms.py
+++ online_store/store/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from .models import Wear
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class WearForm(forms.ModelForm):
     class Meta:
         model = Wear
         fields = ['name', 'size', 'color', 'price', 'category_id']
+
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
